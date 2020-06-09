d627f7d7a6b2f7fe289651a729c8dadea57a4970
vagrant <vagrant@localhost.localdomain>
2020-06-09 12:46:17 +0000

    [Add]second commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,23 +4,43 @@ class BooksController < ApplicationController
 
   def index
   	@books = Book.all
+  	@book = Book.new
   end
 
   def show
   	@book = Book.find(params[:id])
   end
 
-  def new
-  	@book = Book.new
-  end
-
   def create
   	book = Book.new(book_params)
-  	book.save
-  	redirect_to book_path(book.id)
+  	if book.save
+  		flash[:notice] = "Book was successfully created."
+  		redirect_to book_path(book.id)
+  # 	else
+  # 		flash[:alert] = "2 errors prohibited this book from being saved:"
+		# flash.discard(:alert)
+  # 		render :new
+  	end
   end
 
   def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+  	book = Book.find(params[:id])
+  	if book.update(book_params)
+  		flash[:notice] = "Book was successfully updated"
+  		redirect_to book_path(book.id)
+  	end
+  end
+
+  def destroy
+  	book = Book.find(params[:id])
+  	if book.destroy
+  		flash[:notice] = "Book was successfully destroyed."
+  		redirect_to books_path
+  	end
   end
 
   private
